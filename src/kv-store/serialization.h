16ec5dff4f7cf26d9b31e1ba195260594059a86d
Amy Tang <tang.amy@husky.neu.edu>
2020-03-19 19:44:09 -0400

    i did it?

--- src/kv-store/serialization.h
+++ src/kv-store/serialization.h
@@ -3,14 +3,12 @@ class Serialization
 public:
     Serialization() {}
 
-    char *serialize_int(int val)
+    void serialize_int(int val, char *buffer)
     {
-        char buffer[4];
         buffer[0] = (val & 0xff000000) >> 24;
         buffer[1] = (val & 0x00ff0000) >> 16;
         buffer[2] = (val & 0x0000ff00) >> 8;
         buffer[3] = (val & 0x000000ff);
-        return buffer;
     }
 
     int deserialize_int(char *int_buffer)
@@ -20,5 +18,17 @@ public:
         val |= ((int)int_buffer[1]) << 16;
         val |= ((int)int_buffer[2]) << 8;
         val |= ((int)int_buffer[3]);
+        return val;
     }
-}
+
+    void serialize_bool(bool val, char *buffer)
+    {
+        serialize_int((int)val, buffer);
+    }
+
+    bool deserialize_bool(char *buffer)
+    {
+        bool val = (bool)deserialize_int(buffer);
+        return val;
+    }
+};
